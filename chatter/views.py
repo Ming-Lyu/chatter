67de3ef5b2301660f6bcb64d5b31bef637a68ed7
MingLyu <minglyupolimi@gmail.com>
2020-09-21 17:01:10 +0200

    Added styling

--- chatter/views.py
+++ chatter/views.py
@@ -1,10 +1,14 @@
-# chat/views.py
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+import json
 
 def index(request):
     return render(request, 'chatter/index.html', {})
 
+@login_required
 def room(request, room_name):
     return render(request, 'chatter/room.html', {
-        'room_name': room_name
+        'room_name_json': mark_safe(json.dumps(room_name)),
+        'username': mark_safe(json.dumps(request.user.username)),
     })
