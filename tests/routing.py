5946d1be834ce51a8a6eb9ecec9f9f62d9d6d9b9
MingLyu <minglyupolimi@gmail.com>
2020-09-21 10:34:59 +0200

    Added websockets protocol routing

--- tests/routing.py
+++ tests/routing.py
@@ -1,5 +1,12 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chatter import routings
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            routings.websocket_urlpatterns
+        )
+    ),
 })
