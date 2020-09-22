cf0204fe793e781ef0e708a4d37744464038dc5c
MingLyu <minglyupolimi@gmail.com>
2020-09-22 12:09:21 +0200

    Redefined model structure

--- chatter/models.py
+++ chatter/models.py
@@ -7,17 +7,30 @@ from django.conf import settings
 from ckeditor.fields import RichTextField
 from django_fsm import FSMField, transition
 
+
 class Dialog(TimeStampedModel):
     # TODO verbalization
+    participants = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name=_("participants"))
+    title  = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return _(f"dialog-{self.pk}")
+
+
+class Message(TimeStampedModel):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("Dialog owner"),
                               on_delete=models.CASCADE)
     title  = models.CharField(max_length=100)
     body   = RichTextField()
     workflow_state = FSMField(default='draft')
-    
-    def __str__(self):
-        return _(f"{self.owner}'s dialog")
 
+    # attached to one dialog
+    dialog = models.ForeignKey(Dialog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return _(f"{self.owner}'s message")
+
+    
     @transition(field=workflow_state, source='draft', target='sended')
     def send(self):
         pass
