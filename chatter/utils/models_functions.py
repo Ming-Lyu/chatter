e68d0b6214dc1c3d8789b36bb5f8a7a7d0da4903
luigi <luigiliu617@gmail.com>
2020-10-01 20:02:37 +0200

    last message

---
+++ chatter/utils/models_functions.py
@@ -0,0 +1,12 @@
+from django.db.models import Aggregate, CharField, JSONField
+
+class Concat(Aggregate):
+    function = 'GROUP_CONCAT'
+    template = '%(function)s(%(distinct)s%(expressions)s)'
+
+    def __init__(self, expression, distinct=False, **extra):
+        super(Concat, self).__init__(
+            expression,
+            distinct='DISTINCT ' if distinct else '',
+            output_field=JSONField(),
+            **extra)
--- chatter/views.py
+++ chatter/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 import json
 
 from chatter.models import Dialog
+from chatter.utils.models_functions import Concat
 
 def index(request):
     return render(request, 'chatter/index.html', {})
@@ -23,9 +24,15 @@ def room(request, room_pk=None):
     dialog = Dialog.objects.get_or_create_dialog(request.user, opponent=opponent, id=room_pk)
 
     #init
-    id_list = [user.pk for dialog in Dialog.objects.filter(participants=request.user).iterator() for user in dialog.participants.all()]
-    dialog_users = User.objects.filter(id__in=id_list)
-    
+    dialog_users = Dialog.objects.annotate(users = Concat('participants__username')).filter(participants=request.user).latest_message_list().values('users', 'last_message')
+    for dialog_user in dialog_users:
+        if not dialog_user['last_message']:
+            dialog_user['last_message'] = ''
+        if len(dialog_user['users'].split(',')) != 1:
+            users_list = dialog_user['users'].split(',')
+            users_list.remove(request.user.username)
+            dialog_user['users'] = users_list[0]
+
     # TODO this work only 1 on 1
     opponent_username = opponent if opponent else dialog.participants.exclude(pk = request.user.pk).get().username
     message_form = MessageForm()
